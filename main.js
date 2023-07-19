798abbdebe13656e0354694882ee7fda2bc2bf98
Developer Sajid <mdsajidahmed08@gmail.com>
2023-07-19 19:41:22 +0600

    finished

--- main.js
+++ main.js
@@ -132,23 +132,32 @@
 // Answer 8 It seems like you have repeated Snippet 7.
 
 
-B. Anonymous functions
------------------------------------
 
-Instructions:
-1. Create a new JavaScript file named `anonymous_functions.js`.
-2. Write JavaScript code to solve the following tasks using anonymous functions.
-3. Use anonymous functions where specified.
-4. Test your code by running the file in a web browser or a JavaScript runtime environment.
 
-Tasks:
-1. Create an anonymous function that takes two numbers as parameters and returns their sum.
-   Example Input: 3, 5
-   Example Output: 8
+// B. Anonymous functions
+// -----------------------------------
 
-2. Create an array of numbers and use the `map()` method with an anonymous function to square each number in the array.
-   Example Input: [1, 2, 3, 4, 5]
-   Example Output: [1, 4, 9, 16, 25]
+// Instructions:
+// 1. Create a new JavaScript file named `anonymous_functions.js`.
+// 2. Write JavaScript code to solve the following tasks using anonymous functions.
+// 3. Use anonymous functions where specified.
+// 4. Test your code by running the file in a web browser or a JavaScript runtime environment.
+
+// Tasks:
+// 1. Create an anonymous function that takes two numbers as parameters and returns their sum.
+//    Example Input: 3, 5
+//    Example Output: 8
+
+   // Answer is given anonymous_functions.js files
+
+// 2. Create an array of numbers and use the `map()` method with an anonymous function to square each number in the array.
+//    Example Input: [1, 2, 3, 4, 5]
+//    Example Output: [1, 4, 9, 16, 25]
+
+      // Answer is given anonymous_functions.js files
+
+
+
 
 C. JavaScript arrow function 
 
@@ -165,5 +174,6 @@ Tasks:
 // Step 2: Call the calculateSquare function with different numbers
 // Step 3: Print the results to the console
 
-Submission Instruction :
-*** Submit your all file as a pdf once you have completed the tasks. ***
+
+// Submission Instruction :
+// *** Submit your all file as a pdf once you have completed the tasks. ***
